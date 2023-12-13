390135b4f2bc963c67467dcb51c783110aad37f8
DeZero <urbanaspaulius@gmail.com>
2023-12-14 00:28:44 +0200

    part2

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
     $wordsArr = [
-        "0" => "zero",
         "1" => "one",
         "2" => "two",
         "3" => "three",
@@ -15,32 +14,46 @@
     $data = explode("\n", $data);
     $answer = 0;
     for ($i = 0; $i < 1000; $i++) {
-
-        for ($a = 0; $a < count($wordsArr); $a++) {
-             $data[$i] = str_replace($wordsArr[$a], (string) $a, $data[$i]);
-            //echo $data[$i]." ".$a."<br>";
-        }
-
         $arr = str_split($data[$i]);
-        print_r($arr);
-echo "<br>";
         for ($j = 0; $j < count($arr); $j++) {
             if (is_numeric($arr[$j])) {
                 $nr1 = $arr[$j];
                 break;
+            } else {
+                $break = false;
+                foreach ($wordsArr as $key => $word) {
+                    //echo $data[$i].$word.$j." ";
+                    if (strpos($data[$i], $word, $j) === $j) {
+                        $nr1 = (int) $key;
+                        $break = true;
+                        break;
+                    }
+                }
+                if ($break) {
+                    break;
+                }
             }
         }
         $arr = array_reverse($arr);
-        print_r($arr);
-echo "<br>";
         for ($j = 0; $j < count($arr); $j++) {
             if (is_numeric($arr[$j])) {
                 $nr2 = $arr[$j];
                 break;
+            } else {
+                $break = false;
+                foreach ($wordsArr as $key => $word) {
+                    if (strpos(strrev($data[$i]), strrev($word), $j) === $j) {
+                        $nr2 = (int) $key;
+                        $break = true;
+                        break;
+                    }
+                }
+                if ($break) {
+                    break;
+                }
             }
         }
-        $number = $nr1.$nr2;
-        echo $i." ".$answer."+".$number."=".$answer+$number."<br>";
+        $number = $nr1.$nr2."<br>";
         $answer = $answer + $number;
     } 
     echo $answer;
